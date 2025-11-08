01ebedeed6366cd249287636e84fd90f3c763234
Samad10jan <abdulsamad10jan@gmail.com>
2025-11-08 16:20:10 +0530

    first commit

---
+++ 1/index.js
@@ -0,0 +1,37 @@
+// console.log("chai or code");
+require('dotenv').config()
+const express = require('express')
+// import express from "express" same
+
+const app = express() // all functinality of express
+
+// const port = 3000 // open in your computer
+
+// get request on "/" and req, res
+app.get('/', (req, res) => {
+  res.send('Hello World!') //sending string
+})
+
+//
+app.get('/twitter',(req,res)=>{
+    res.send('username') 
+})
+
+// HotReLoading?
+// when implement new thing during running server , it will not show untill you restart your sever " npm run start"
+app.get('/login',(req,res)=>{
+    res.send('<h1>ABDUL</h1>') //sending tag, show in bold large letter 
+})
+app.get('/getData', (req, res) => {
+  res.json({}) //sending string
+})
+
+// listen
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+// Deploy:
+// digital ocean
+// seenode
+// render
+// cyclic
